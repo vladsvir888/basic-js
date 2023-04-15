2cd6fa08682fdc755b1667c2f257c3f5076ee654
Vladislav Svirydovich <vladsvir888@gmail.com>
2023-04-15 19:17:03 +0300

    feat: add count-cats and delete-digit

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  const res = matrix.flat(Infinity).filter(item => item === '^^');
+
+  return !res.length ? 0 : res.length;
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const arr = String(n).split('').map(Number);
+
+  const copy = [...arr];
+  
+  if (copy.length < 3) {
+    arr.forEach((item, index) => {
+      if (item > arr[index + 1]) {
+        copy.splice(1, 1);
+      }
+    });
+  } else {
+    arr.forEach((item, index) => {
+      if (item < arr[index + 1]) {
+        copy.splice(index, 1);
+      }
+    });
+  }
+
+  return Number(copy.join(''));
 }
 
 module.exports = {
